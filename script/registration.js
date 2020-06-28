d969a4d01a8d33c4002a641498e58ab533e2563a
R.B. Horn <51953918+rowanhorn1412@users.noreply.github.com>
2020-06-29 01:24:59 +0200

    Add files via upload
    finishing touches

--- script/registration.js
+++ script/registration.js
@@ -1,3 +1,6 @@
+
+let textvalidate = false;
+let fieldvalidate = false;
 let fieldset = document.getElementsByTagName('fieldset');
 let form = document.querySelector('form');
 let firstname = document.getElementById('firstname');
@@ -5,34 +8,41 @@ let lastname = document.getElementById('lastname');
 let email = document.getElementById('email');
 let password = document.getElementById('password');
 let submit = document.getElementById('submit');
-let textvalidate = false;
-let fieldvalidate = false;
+let text = /^[A-Za-z]\w{7,14}$/;
+let numbers = /^[0-9]+$/;
+let passwordCharacters = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
 
 
-// Hidding the second fieldset
+// Hiding the second fieldset
 fieldset[1].style.display = "none";
-// Hidding the submit Button
+// Hiding the submit Button
 submit.style.display = "none";
 
+// Checks if the first name & last name don't have any numbers in the name
 function textValidation() {
-    if (firstname.value && lastname.value) {
+    if (firstname.value.match(numbers) && lastname.value.match(numbers)) {
+        alert('Geen cijfers gebruiken bij namen');
     } else {
         textvalidate = true;
     }
 }
 
+// password uses all type of characters
+function passwordChecker() {
+    if (password.value.match(passwordCharacters)) {
+        textvalidate = true;
+    }
+}
+
+// Hides the second fieldset
 function fieldsetOne() {
     if (textvalidate == true && password.value) {
         fieldvalidate = true;
     }
 }
 
-function fieldsetTwo() {
-    if (textvalidate == true && lastname.value) {
-        fieldvalidate = true;
-    }
-}
-
+// will display the second fieldset
+// Will also display the submit button
 form.addEventListener('keydown', function() {
     textValidation();
     fieldsetOne();
