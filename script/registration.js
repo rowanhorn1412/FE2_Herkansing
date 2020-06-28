b3046a2e3c8444db35d56473b630c415a8e0e6fc
R.B. Horn <51953918+rowanhorn1412@users.noreply.github.com>
2020-06-28 23:08:13 +0200

    Add files via upload

--- script/registration.js
+++ script/registration.js
@@ -7,58 +7,38 @@ let password = document.getElementById('password');
 let submit = document.getElementById('submit');
 let textvalidate = false;
 let fieldvalidate = false;
-let passwordCharacters = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
 
 
-email.addEventListener('keyup', function (event) {
-    isValidEmail = email.checkValidity();
-    
-    if ( isValidEmail ) {
-      okButton.disabled = false;
+// Hidding the second fieldset
+fieldset[1].style.display = "none";
+// Hidding the submit Button
+submit.style.display = "none";
+
+function textValidation() {
+    if (firstname.value && lastname.value) {
     } else {
-      okButton.disabled = true;
+        textvalidate = true;
     }
-  });
+}
 
-  button.addEventListener('click', function (event) {
-    registration.submit();
-  });
+function fieldsetOne() {
+    if (textvalidate == true && password.value) {
+        fieldvalidate = true;
+    }
+}
 
-// // Verbergen van 2e fieldset
-// fieldset[1].style.display = "none";
-// submit.style.display = "none";
+function fieldsetTwo() {
+    if (textvalidate == true && lastname.value) {
+        fieldvalidate = true;
+    }
+}
 
-// // Verbergen van 3e fieldset
-// fieldset[2].style.display = "none";
-// submit.style.display = "none";
-
-// function valideertext() {
-//     if (firstname.value.match(nummers) && achternaam.value.match(nummers)) {
-//         alert('Geen cijfers gebruiken bij namen');
-//     } else {
-//         textvalidate = true;
-//     }
-// }
-
-// function valideerfieldset1() {
-//     if (textvalidate == true && email.value) {
-//         fieldvalidate = true;
-//     }
-// }
-
-// function valideerfieldset2() {
-//     if (textvalidate == true && gender.value) {
-//         fieldvalidate = true;
-//     }
-// }
-
-// form.addEventListener('keydown', function() {
-//     valideertext();
-//     valideerfieldset1();
-
-//     if (textvalidate == true && fieldvalidate == true) {
-//         fieldset[1].style.display = "block";
-//     } else {
-
-//     }
-// });
+form.addEventListener('keydown', function() {
+    textValidation();
+    fieldsetOne();
+    if (textvalidate == true && fieldvalidate == true) {
+        fieldset[1].style.display = "block";
+        submit.style.display = "block";
+    } else {
+    }
+});
